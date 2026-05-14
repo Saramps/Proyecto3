212495888cecb1e4c8c5f8eb17a5295de630a8bc
Saramps <s.palacioss@uniandes.edu.co>
2026-05-14 18:03:34 -0500

    ...

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ app = FastAPI(title="Dann-Alpes Reviews API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"]
 )
 
